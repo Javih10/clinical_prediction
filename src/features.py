fed7e02fd0bcdb35b83546a69998a88d64c1f19a
Javier Hernandez <javierh2045@gmail.com>
2025-12-18 17:43:50 -0600

    Adding descriptions to functions

--- src/features.py
+++ src/features.py
@@ -14,13 +14,13 @@ nums = (
 )
 
 def renaming_cols(df):
-    """_summary_
+    """Renaming some columns to make them similar to others 
 
     Args:
-        df (_type_): _description_
+        df (dataframe): the data being worked with in the form of a df
 
     Returns:
-        _type_: _description_
+        dataframe: a dataframe with some columns being renamed
     """
     
     df = df.copy()
@@ -32,13 +32,13 @@ def renaming_cols(df):
     return df 
 
 def total_visits_col(df):
-    """_summary_
+    """Creates a new column with total number of visits per patient
 
     Args:
-        df (_type_): _description_
+        df (dataframe): dataframe with the columns 'num_inpatient','num_emergency','num_outpatient'
 
     Returns:
-        _type_: _description_
+        dataframe: returns a dataframe with a new column total_visits
     """
     
     df = df.copy()
@@ -49,13 +49,13 @@ def total_visits_col(df):
 
 
 def medication_changes(df):
-    """_summary_
+    """Returns whether the medication has changed and how many times it has 
 
     Args:
-        df (_type_): _description_
+        df (dataframe): dataframe with columns that contains medications and whether used on the patient
 
     Returns:
-        _type_: _description_
+        dataframe: a dataframe with a new column describing num_medication_change
     """
     df = df.copy()
     
@@ -64,13 +64,13 @@ def medication_changes(df):
     return df 
 
 def usage_of_insulin(df):
-    """_summary_
+    """Returns whether insulin was used on the patient with 1 for yes and 0 for no 
 
     Args:
-        df (_type_): _description_
+        df (dataframe): dataframe with a column named 'insulin'
 
     Returns:
-        _type_: _description_
+        dataframe: a dataframe with a new column insulin_used
     """
     
     df = df.copy()
@@ -81,12 +81,23 @@ def usage_of_insulin(df):
 
 
 def has_complication(code):
+    """Returns whether a patient has complications with any diagnoses 
+
+    """
     if pd.isna(code):
         return 0
     return int(any(str(code).startswith(prefix) for prefix in nums))
 
 
 def add_has_diabetes_complications(df):
+    """Whether a patient has diabtes complications or not adding three different diagnoses together
+
+    Args:
+        df (dataframe): A dataframe with variables called diag_1, diag_2, and diag_3
+
+    Returns:
+        dataframe: return a column with either 1 (has diabetes effect) or 0 (does not have diabetes effect)
+    """
     df = df.copy()
     df['has_diabetes_complications'] = (
         df[['diag_1', 'diag_2', 'diag_3']]
@@ -95,6 +106,14 @@ def add_has_diabetes_complications(df):
     return df
 
 def adding_all_features(df):
+    """Adding most of the features together into just one function
+
+    Args:
+        df (dataframe): dataframe
+
+    Returns:
+        dataframe: a dataframe with functions applied for getting new features
+    """
     
     df = renaming_cols(df)
     df = total_visits_col(df)
@@ -105,5 +124,6 @@ def adding_all_features(df):
     return df
 
 def drop_col_for_train(df):
+    
     df = df.copy()
     return df.drop(columns = ['encounter_id', 'patient_nbr'])
