8d9fcd1e6296e86eac07c7fcaec8830061bc79ec
Javier Hernandez <javierh2045@gmail.com>
2025-12-18 07:56:48 -0600

    Adding singular function for all functions

---
+++ src/dataset.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+from data.raw.downloaded_data import load_raw_data
+from src.cleaning import drop_unwanted_columns, replacing_missing_values, readmission_label, correcting_col_types 
+from src.features import adding_all_features, readmission_target, drop_col_for_train
+
+def finalizing_dataset(df):
+    df = load_raw_data(df)
+    df = drop_unwanted_columns(df)
+    df = replacing_missing_values(df)
+    df = readmission_label(df)
+    df = correcting_col_types(df)
+    df = adding_all_features(df)
+    df = readmission_target(df)
+    df = drop_col_for_train(df)
+    
+    return df
