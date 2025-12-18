d9bc6141b0b138f80e2bad2f3f7cadfaf4e0a8c0
Javier Hernandez <javierh2045@gmail.com>
2025-12-18 12:28:23 -0600

    adding features to train ml models

---
+++ src/preprocessing.py
@@ -0,0 +1,44 @@
+import pandas as pd 
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.pipeline import Pipeline
+
+
+def column_type(X):
+    """
+
+    Args:
+        X (_type_): _description_
+    """
+    
+    num_cols = X.select_dtypes(
+        include=['int64', 'float64']
+    ).columns.tolist()
+    
+    cat_cols = X.select_dtypes(
+        include=['object','category']
+    ).columns.tolist()
+    
+    return cat_cols, num_cols
+
+
+def building_pipeline(cat_cols, num_cols):
+    num_trans = Pipeline(steps=[
+        ('scaler', StandardScaler())
+    ])
+    
+    cat_trans = Pipeline(steps=[
+        ('onehot', OneHotEncoder(
+            handle_unknown='ignore',
+            sparse=False
+        ))
+    ])
+    
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', num_trans, num_cols),
+            ('cat', cat_trans, cat_cols)
+        ]
+    )
+    
+    return preprocessor
